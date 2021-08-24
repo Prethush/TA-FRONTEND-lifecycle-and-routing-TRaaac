7a5696d46c9132d758d06238d85a72526b27dd8b
Prethush <ktmirash49@.gmail.com>
2021-08-24 12:28:25 +0530

    Random Person

---
+++ block-BRaaby/my-app/src/components/Loader.js
@@ -0,0 +1,14 @@
+import "../style/loader.css";
+
+function Loader() {
+   return (
+    <div className="bouncing-loader h-screem flex flex-col justify-center items-center">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+   )
+}
+
+
+export default Loader;
